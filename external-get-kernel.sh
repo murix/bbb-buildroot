1648ece4711e9fccbc9df6b11e778efa0dc0fe10
murilopontes@gmail.com <murix>
2014-07-20 14:18:52 -0300

    config

--- external-get-kernel.sh
+++ external-get-kernel.sh
@@ -4,7 +4,7 @@ git checkout 3.8
 ./patch.sh
 cp configs/beaglebone kernel/arch/arm/configs/beaglebone_defconfig
 wget http://arago-project.org/git/projects/?p=am33x-cm3.git\;a=blob_plain\;f=bin/am335x-pm-firmware.bin\;hb=HEAD -O kernel/firmware/am335x-pm-firmware.bin
-cd kernel
+#cd kernel
 #make ARCH=arm CROSS_COMPILE=arm-linux-gnu- beaglebone_defconfig
 #make ARCH=arm CROSS_COMPILE=arm-linux-gnu- uImage dtbs
 #make ARCH=arm CROSS_COMPILE=arm-linux-gnu- uImage-dtb.am335x-boneblack
